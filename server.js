c590dfee3d2db5b72ef0766ca40b78160f8ae2fd
Hans <sanpascualh73@gmail.com>
2023-11-24 14:34:28 +0800

    mod server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 
 
 app.use(express.json({ limit: '100mb' }))
-app.use(express.urlencoded({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use(cors())
 
