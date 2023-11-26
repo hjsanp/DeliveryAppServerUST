675e71d1957e9a77b442d778da34de5eb7b454a1
Hans <sanpascualh73@gmail.com>
2023-11-27 00:59:52 +0800

    mod favorites

--- routes/modData.js
+++ routes/modData.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { addAddress, getAddresses, getOrders, submitOrder, addFavorite, } = require('../controllers/modData')
+const { addAddress, getAddresses, getOrders, submitOrder, addFavorite, getFavorites } = require('../controllers/modData')
 
 router.route('/addAddress').post(addAddress)
 router.route('/getAddresses').post(getAddresses)
