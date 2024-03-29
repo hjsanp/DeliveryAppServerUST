c887cb1ce3e74808eab985e58cb5bc553e82b069
Hans <sanpascualh73@gmail.com>
2024-03-29 12:20:24 +0800

    mod Order Model

--- models/Order.js
+++ models/Order.js
@@ -14,6 +14,7 @@ const OrderSchema = new mongoose.Schema({
     finalPrice: Number,
     discountedPrice: Number,
     img: String,
+    restaurantName: String,
     cart: {
         type: Map,
         of: {},
